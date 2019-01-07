e7ac0a80f6f6771cc96d9f89f4c8cd531d8156cd
Elisha Hastings <5149755+tshakah@users.noreply.github.com>
2019-01-07 13:52:06 +1300

    Various changes to fish config

--- config.fish
+++ config.fish
@@ -8,8 +8,9 @@ set -g theme_display_vi yes
 set -g theme_display_hg yes
 set -g theme_show_exit_status yes
 set -g theme_color_scheme gruvbox
-set -Ux BAT_THEME gruvbox
 set --universal fish_user_paths $fish_user_paths ~/.npm/node_modules/bin/
+set -Ux BAT_THEME gruvbox
+set -gx EDITOR nvim
 
 fish_vi_key_bindings
 fish_default_key_bindings -M insert
@@ -20,6 +21,18 @@ bind -M insert \e\[1\;5D backward-word
 bind -M visual \e\[1\;5C forward-word
 bind -M visual \e\[1\;5D backward-word
 
+alias nix-shell "nix-shell --run fish"
 alias rg "rg -S -M 200 --glob '!vendor' --glob '!data'"
-alias vi "nvim"
-alias vpn='z vpn; and sudo openvpn capvpn.ovpn'
+alias vpn 'z vpn; and sudo openvpn capvpn.ovpn'
+alias vi 'nvim'
+alias ls 'ls --color=tty'
+alias hope 'z hope; and nix-shell'
+alias imbr 'z imbr; and nix-shell'
+alias aire 'z aire; and nix-shell'
+alias ddw 'z worker; and nix-shell'
+alias dash 'z dev_dashboard; and nix-shell'
+alias our 'z adventure; and nix-shell'
+alias dot 'z dotfiles'
+alias scr 'z scripts'
+
+source ~/.config/fish/gnupg.fish
