ad91d5384fe49e7721a9c9253fcbff66be0039a8
Elisha Hastings <5149755+tshakah@users.noreply.github.com>
2022-02-08 13:48:16 +0000

    Stop treesitter parsing Vue
    Vue crashes nvim - https://github.com/nvim-treesitter/nvim-treesitter/issues/2360

--- vim/init.lua
+++ vim/init.lua
@@ -159,7 +159,7 @@ vim.lsp.with(
 -- Set up treesitter
 require'nvim-treesitter.configs'.setup {
   ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  ignore_install = { }, -- List of parsers to ignore installing
+  ignore_install = { "vue" }, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = { "elixir" }, -- list of language that will be disabled
