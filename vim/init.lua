d721a3db432327771543cebf3bf6863f2ec030d3
Elisha Hastings <5149755+tshakah@users.noreply.github.com>
2022-02-08 13:47:53 +0000

    Fix deprecation

--- vim/init.lua
+++ vim/init.lua
@@ -107,7 +107,7 @@ local on_attach = function(client)
   buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
   buf_set_keymap("n", "<space>k", "<cmd>CodeActionMenu<CR>", opts)
   buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-  buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+  buf_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
   buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
   buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
 
