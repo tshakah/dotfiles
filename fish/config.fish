7a5c55f9ddb6857c7d67641e9a168c9e3786cc5e
Elisha Hastings <5149755+tshakah@users.noreply.github.com>
2022-04-03 10:18:02 +0100

    Add fuzzy history deletion

--- fish/config.fish
+++ fish/config.fish
@@ -84,3 +84,8 @@ end
 
 function fish_greeting
 end
+
+# https://jordanelver.co.uk/blog/2020/05/29/history-deleting-helper-for-fish-shell/
+function dh -d "Fuzzily delete entries from your history"
+  history | fzf | read -l item; and history delete --prefix "$item"
+end
