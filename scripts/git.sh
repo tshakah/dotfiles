581a861faa8011fa823743c4043bad25d16e1c8e
Elisha Hastings <tshakah@gmail.com>
2022-12-22 14:24:29 +0000

    Tidy up git bash

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -o pipefail
+
+if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
+
 # This is intended to be used as an alias for git (e.g. `alias git '~/source/dotfiles/scripts/git.sh'`), and checks some
 # of the git commands to avoid common mistakes in everyday use. It has to be an alias as git doesn't have hooks to check
 # these commands.
@@ -10,21 +14,21 @@ function check_parent_branch {
     # If the requested branch exists, just check it out
     git show-ref --verify --quiet refs/heads/"$1"
 
-    if [ $? == 0 ]; then
-        git checkout $1
+    if [[ "$?" == 0 ]]; then
+        git checkout "$1"
         exit 0
     fi
 
     # Otherwise shift off the requested branch (storing it for later) and continue
-    newbranch=$1
+    local newbranch="$1"
     shift
 
     # Get the current branch
-    branch="$(git rev-parse --abbrev-ref HEAD)"
+    local branch="$(git rev-parse --abbrev-ref HEAD)"
 
     # Make sure we actually want to use this branch as the parent of the new one
     #(if we aren't branching off main/master).
-    if [[ $branch != "master" ]] && [[ $branch != "main" ]]; then
+    if [[ "$branch" != "master" ]] && [[ "$branch" != "main" ]]; then
         while read -p "`echo -e '\e[1m'`You are branching off `echo -e '\e[33m'`$branch`echo -e '\e[0m'`.
 Are there any earlier branches that you could use (y/n)?
 `echo -e '\e[0m';`" yn; do
@@ -38,7 +42,7 @@ Are there any earlier branches that you could use (y/n)?
                     command git "$@";
 
                     # Now ask about tracking origin
-                    ask_about_origin_tracking $newbranch
+                    ask_about_origin_tracking "$newbranch"
 
                     break
                 ;;
@@ -52,7 +56,7 @@ Are there any earlier branches that you could use (y/n)?
         command git "$@"
 
         # Also ask about tracking origin
-        ask_about_origin_tracking $newbranch
+        ask_about_origin_tracking "$newbranch"
     fi
 }
 
@@ -67,7 +71,7 @@ Do you want to set up and track `echo -e '\e[33m'`$1`echo -e '\e[0m\e[1m'` on or
                 echo -e ''
                 command git push -u origin $1;
 
-                if [ $? == 0 ]; then
+                if [[ $? == 0 ]]; then
                     echo -e '\n\e[35m\e[1mDone ♥\e[0m\n';
                     exit 0
                 else
@@ -101,7 +105,7 @@ Do you want to use a safer force push, to prevent remote work being overwritten 
                 shift
                 command git push --force-with-lease --force-if-includes "$@";
 
-                if [ $? == 0 ]; then
+                if [[ $? == 0 ]]; then
                     echo -e '\n\e[35m\e[1mDone ♥\e[0m';
                     exit 0
                 else
@@ -123,24 +127,24 @@ Do you want to use a safer force push, to prevent remote work being overwritten 
     done
 }
 
-if [[ $1 == "psuh" ]]; then
+if [[ "$1" == "psuh" ]]; then
     # I frequently mistype `git push` as `git psuh`
     shift # remove the "psuh"
     # Call the correct command with the rest of the arguments
-    $0 push $@
-elif [[ $1 == "push" && ($2 == "-f" || $2 == "--force") ]]; then
+    "$0" push "$@"
+elif [[ "$1" == "push" && ("$2" == "-f" || "$2" == "--force") ]]; then
     # It would be good to switch the argument checking to named - https://unix.stackexchange.com/a/580258/496695
     safer_force_push_prompt "$@"
-elif [[ $1 == "exdif" || $1 == "exdiff" ]]; then
+elif [[ "$1" == "exdif" || "$1" == "exdiff" ]]; then
     # Show the exclusive diff for a commit - i.e. only what that commit changed. Uses the supplied (or current) commit.
     commit="${2-$(git rev-parse HEAD)}"
     command git diff $commit~ $commit
-elif [[ $1 == "cleanup" ]]; then
-    command git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D $branch; done
+elif [[ "$1" == "cleanup" ]]; then
+    command git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D "$branch"; done
 elif [[ "$1" == "children" ]]; then
     # Find immediate children of the supplied (or current) commit
     parent="${2-$(git rev-parse HEAD)}"
-    for commit in $(git rev-parse $parent^0); do
+    for commit in $(git rev-parse "$parent"^0); do
         for child in $(git log --format='%H %P' --all | grep -F " $commit" | cut -f1 -d' '); do
             git show -s $child
         done
