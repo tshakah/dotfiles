be9be0703e2a5c294769c5f38eda1eb815e16aea
Elisha Hastings <tshakah@gmail.com>
2024-01-10 22:28:18 +0000

    Tidy up git.sh
    This is just following up on the previous commit that set up
    bash-language-server and shellcheck, implementing shellcheck's changes
    for this script

--- scripts/git.sh
+++ scripts/git.sh
@@ -12,9 +12,7 @@ if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 # This tries to prevent branch creation with the wrong parent, e.g. off a feature branch.
 function check_parent_branch {
     # If the requested branch exists, just check it out
-    git show-ref --verify --quiet refs/heads/"$1"
-
-    if [[ "$?" == 0 ]]; then
+    if git show-ref --verify --quiet refs/heads/"$1"; then
         git checkout "$1"
         exit 0
     fi
@@ -24,14 +22,15 @@ function check_parent_branch {
     shift
 
     # Get the current branch
-    local branch="$(git rev-parse --abbrev-ref HEAD)"
+    local branch
+    branch="$(git rev-parse --abbrev-ref HEAD)"
 
     # Make sure we actually want to use this branch as the parent of the new one
     #(if we aren't branching off main/master).
     if [[ "$branch" != "master" ]] && [[ "$branch" != "main" ]]; then
-        while read -p "`echo -e '\e[1m'`You are branching off `echo -e '\e[33m'`$branch`echo -e '\e[0m'`.
+      while read -r -p "$(echo -e '\e[1m')You are branching off $(echo -e '\e[33m')$branch$(echo -e '\e[0m').
 Are there any earlier branches that you could use (y/n)?
-`echo -e '\e[0m';`" yn; do
+$(echo -e '\e[0m';)" yn; do
             case ${yn:0:1} in
                 Y|y )
                     echo -e '\n\e[31m\e[1mCANCELLING\e[0m\n';
@@ -63,15 +62,14 @@ Are there any earlier branches that you could use (y/n)?
 # When creating a branch we should also ask if the user wants to create/track the origin branch, as it's annoying that
 # git doesn't already do this
 function ask_about_origin_tracking {
-    while read -p "`echo -e '\e[1m'`
-Do you want to set up and track `echo -e '\e[33m'`$1`echo -e '\e[0m\e[1m'` on origin as well (y/n)?
-`echo -e '\e[0m'`" yn; do
+    while read -r -p "$(echo -e '\e[1m')
+Do you want to set up and track $(echo -e '\e[33m')$1$(echo -e '\e[0m\e[1m') on origin as well (y/n)?
+$(echo -e '\e[0m')" yn; do
         case ${yn:0:1} in
             Y|y )
                 echo -e ''
-                command git push -u origin $1;
 
-                if [[ $? == 0 ]]; then
+                if command git push -u origin "$1"; then
                     echo -e '\n\e[35m\e[1mDone ♥\e[0m\n';
                     exit 0
                 else
@@ -99,11 +97,11 @@ if [[ "$1" == "psuh" ]]; then
     "$0" push "$@"
 elif [[ $1 == 'push' ]]; then
     # If force pushing, make sure it's with a lease
-    command git $(echo $@ | sed -E 's/\s(-f|--force)/ --force-with-lease/g')
+    command git "$(echo "$@" | sed -E 's/\s(-f|--force)/ --force-with-lease/g')"
 elif [[ "$1" == "exdif" || "$1" == "exdiff" ]]; then
     # Show the exclusive diff for a commit - i.e. only what that commit changed. Uses the supplied (or current) commit.
     commit="${2-$(git rev-parse HEAD)}"
-    command git diff $commit~ $commit
+    command git diff "$commit"~ "$commit"
 elif [[ "$1" == "cleanup" ]]; then
     command git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D "$branch"; done
 elif [[ "$1" == "children" ]]; then
@@ -111,7 +109,7 @@ elif [[ "$1" == "children" ]]; then
     parent="${2-$(git rev-parse HEAD)}"
     for commit in $(git rev-parse "$parent"^0); do
         for child in $(git log --format='%H %P' --all | grep -F " $commit" | cut -f1 -d' '); do
-            git show -s $child
+            git show -s "$child"
         done
     done
 elif [[ "$1" == "branch" && "$2" == "list" ]]; then
