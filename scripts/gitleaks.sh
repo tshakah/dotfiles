c1945e34f1f5c28540c4a2385428a48dc546c816
Elisha Hastings <tshakah@gmail.com>
2024-01-15 16:24:44 +0000

    Update gitleaks script
    - Fix check on gitleaks config
    - Fix output when gitleaks fails
    - Add output if gitleaks is disabled
    - Add linebreak if gitleaks passes

--- scripts/gitleaks.sh
+++ scripts/gitleaks.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-if git config --bool hooks.gitleaks; then
+if [[ $(git config --bool hooks.gitleaks) =~ "true" ]]; then
   if ! gitleaks protect -c ~/source/prima/appsec-automation/sast/sast-image/gitleaks/custom-config.toml -v --staged --redact; then
-    echo -e "Warning: gitleaks has detected sensitive information in your changes.\n"
-            "To disable the gitleaks precommit hook run the following command:\n\n"
-            "git config hooks.gitleaks false"
+    echo -e "\n\e[31m\e[1mWarning:\e[0m\e[1m gitleaks has detected sensitive information in your changes."
+    echo -e "To disable the gitleaks precommit hook run the following command:\n"
+    echo -e "\e[33mgit config hooks.gitleaks false\e[0m\n"
 
     exit 1
   fi
+  echo -e ""
+else
+  echo -e "\n\e[33m\e[1mgitleaks precommit hook disabled!\e[0m\e[1m"
+  echo -e "You can enable with \e[32mgit config hooks.gitleaks true\e[0m\n"
 fi
