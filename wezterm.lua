0f9c102649424865d4ba99ef8a3d90a97bcd1a20
Elisha Hastings <tshakah@gmail.com>
2024-04-23 16:39:58 +0100

    Add wezterm config

---
+++ wezterm.lua
@@ -0,0 +1,30 @@
+local wezterm = require 'wezterm'
+local config = {
+  color_scheme = 'Everforest Dark (Gogh)',
+  enable_tab_bar = false,
+  front_end = "OpenGL",
+  font = wezterm.font('Inconsolata Nerd Font'),
+  font_size = 13,
+  check_for_updates = false,
+  scrollback_lines = 100000,
+  window_padding = {
+    left = 2,
+    right = 2,
+    top = 0,
+    bottom = 0,
+  }
+}
+
+config.ssh_domains = {
+  {
+    -- This name identifies the domain
+    name = 'waldorf.local',
+    -- The hostname or address to connect to. Will be used to match settings
+    -- from your ssh config file
+    remote_address = '10.10.10.1',
+    -- The username to use on the remote host
+    username = 'elishahastings',
+  },
+}
+
+return config
