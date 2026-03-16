d5510b489d9a01c9fe11d711b2c7607872e43864
Elisha Hastings <5149755+tshakah@users.noreply.github.com>
2026-03-16 14:31:25 +0000

    Autoformat wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -1,46 +1,51 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 local config = {
-  color_scheme = 'Everforest Dark (Gogh)',
-  enable_tab_bar = false,
-  front_end = "WebGpu",
-  font = wezterm.font('Inconsolata Nerd Font'),
-  font_size = 13,
-  check_for_updates = false,
-  scrollback_lines = 100000,
-  force_reverse_video_cursor = true,
-  window_padding = {
-    left = 2,
-    right = 2,
-    top = 0,
-    bottom = 0,
-  }
+	color_scheme = "Everforest Dark (Gogh)",
+	enable_tab_bar = false,
+	front_end = "OpenGL",
+	font = wezterm.font("Inconsolata Nerd Font"),
+	font_size = 13,
+	check_for_updates = false,
+	scrollback_lines = 100000,
+	force_reverse_video_cursor = true,
+	window_padding = {
+		left = 2,
+		right = 2,
+		top = 0,
+		bottom = 0,
+	},
 }
 
 config.keys = {
-  { key = 'UpArrow',   mods = 'SHIFT', action = act.ScrollToPrompt(-1) },
-  { key = 'DownArrow', mods = 'SHIFT', action = act.ScrollToPrompt(1) },
+	{
+		key = "UpArrow",
+		mods = "SHIFT",
+		action = act.ScrollToPrompt(-1),
+	},
+	{ key = "Enter", mods = "SHIFT", action = wezterm.action({ SendString = "\x1b\r" }) },
+	{ key = "DownArrow", mods = "SHIFT", action = act.ScrollToPrompt(1) },
 }
 
 config.mouse_bindings = {
-  {
-    event = { Down = { streak = 4, button = 'Left' } },
-    action = wezterm.action.SelectTextAtMouseCursor 'SemanticZone',
-    mods = 'NONE',
-  },
+	{
+		event = { Down = { streak = 4, button = "Left" } },
+		action = wezterm.action.SelectTextAtMouseCursor("SemanticZone"),
+		mods = "NONE",
+	},
 }
 
 config.ssh_domains = {
-  {
-    -- This name identifies the domain
-    name = 'waldorf.local',
-    -- The hostname or address to connect to. Will be used to match settings
-    -- from your ssh config file
-    remote_address = '10.10.10.1',
-    -- The username to use on the remote host
-    username = 'elishahastings',
-  },
+	{
+		-- This name identifies the domain
+		name = "waldorf.local",
+		-- The hostname or address to connect to. Will be used to match settings
+		-- from your ssh config file
+		remote_address = "10.10.10.1",
+		-- The username to use on the remote host
+		username = "elishahastings",
+	},
 }
 
 return config
